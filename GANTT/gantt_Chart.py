f42d23728a28f5abc65aec36f8f7f2abbc8210f8
mmaurostoffel <166130318+mmaurostoffel@users.noreply.github.com>
2025-04-12 11:16:27 +0200

    Update GANNT

--- GANTT/gantt_Chart.py
+++ GANTT/gantt_Chart.py
@@ -23,14 +23,14 @@ Ende Master 25.Juli
 
 # Sample task data with (Task Name, Start Date, End Date)
 tasks = [
-    ("Resultat/Doku", "5-7-2025", "25-7-2025"),
-    ("Test Endprodukt", "14-6-2025", "5-7-2025"),
-    ("Endprodukt", "7-6-2025", "14-6-2025"),
-    ("Test Path-Finding", "31-5-2025", "7-6-2025"),
-    ("Path-Finding", "24-5-2025", "31-5-2025"),
-    ("Adjazenzmatrizen", "17-5-2025", "24-5-2025"),
-    ("Ausgangslagen", "10-5-2025", "17-5-2025"),
-    ("Zellautomat", "5-5-2025", "10-5-2025"),
+    ("Resultat/Doku", "5-5-2025", "25-7-2025"),
+    ("Test Endprodukt", "12-6-2025", "5-7-2025"),
+    ("Endprodukt", "7-6-2025", "17-6-2025"),
+    ("Test Path-Finding", "30-5-2025", "7-6-2025"),
+    ("Path-Finding", "24-5-2025", "1-6-2025"),
+    ("Adjazenzmatrizen", "5-5-2025", "24-5-2025"),
+    ("Ausgangslagen", "5-5-2025", "20-5-2025"),
+    ("Zellautomat", "5-5-2025", "17-5-2025"),
 ]
 # Convert string dates to datetime objects
 def str_to_date(date_str):
@@ -51,10 +51,15 @@ for i, (task, start, end) in enumerate(tasks):
 ax.xaxis.set_major_locator(mdates.DayLocator(interval=7))  # Tick every 5 days
 ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
 ax.axvline(str_to_date("25-7-2025"), color='red', linestyle="--", linewidth=2, label="Ende Master")
+plt.text(str_to_date("14-7-2025"),6.5,"Meilenstein 3:\nEnde Master\n", c='r')
+ax.axvline(str_to_date("07-06-2025"), color='red', linestyle="--", linewidth=2, label="Abschluss der Zellautomaten")
+plt.text(str_to_date("08-06-2025"),6.5,"Meilenstein 2:\nAbschluss der \nZellautomaten", c='r')
+ax.axvline(str_to_date("24-05-2025"), color='red', linestyle="--", linewidth=2, label="Abschluss des Path-Finding")
+plt.text(str_to_date("25-05-2025"),6.5,"Meilenstein 1:\nAbschluss des \nPath-Finding", c='r')
 
 plt.xticks(rotation=45)
 plt.xlabel("Datum")
-plt.ylabel("Meilensteine")
+plt.ylabel("Arbeitsschritte")
 plt.title("Meilensteine Masterarbeit")
 plt.grid(axis='x', linestyle='--', alpha=0.7)
 
