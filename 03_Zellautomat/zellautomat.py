aca3a61b8d8479cbbdecdb018505ec86c5bf6f2f
mmaurostoffel <166130318+mmaurostoffel@users.noreply.github.com>
2025-05-19 20:11:39 +0200

    Basic Zellautomat abgeschlossen

--- 03_Zellautomat/zellautomat.py
+++ 03_Zellautomat/zellautomat.py
@@ -20,33 +20,50 @@ new = old.copy()
 GRIDSIZE_X = emptyMap.shape[0]
 GRIDSIZE_Y = emptyMap.shape[1]
 
+
+################## Temporary ##################
+EXIT_X = GRIDSIZE_X
+EXIT_Y = GRIDSIZE_Y / 2
+################## Temporary ##################
+
 # Read starting Positions from scenario file
 scenario = pd.read_csv("../02_createScenarios/scenario_files/altstadt_50_5_2025-5-18-13%10.csv")
 for index,row in scenario.iterrows():
     x,y = row["startingPos"].strip("[]").split(",")
     old[int(x), int(y)] = CELL_PED
 
+#def update(old, new):
+#    print("")
 
 def update(old, new):
-    print("")
-
-def update(old, new):
-    for x in range(GRIDSIZE_X):
-        for y in range(GRIDSIZE_Y):
+    for x in range(1,GRIDSIZE_X-1):
+        for y in range(1,GRIDSIZE_Y-1):
             if old[x, y] == CELL_PED:
-                if old[(x + 1) % GRIDSIZE_X, y] == CELL_EMP:
-                    new[(x + 1) % GRIDSIZE_X, y] = CELL_PED
-                    old[(x + 1) % GRIDSIZE_X, y] = CELL_OBS
-                else:
-                    if old[x, y] == CELL_EMP:
-                        new[x, y] = CELL_PED
-                        old[x, y] = CELL_OBS
-                    elif old[x, y] == CELL_EMP:
-                        new[x, y] = CELL_PED
-                        old[x, y] = CELL_OBS
+                x_dist = x-EXIT_X
+                x_move = 1 if x_dist > 0 else -1
+                y_dist = y-EXIT_Y
+                y_move = 1 if y_dist > 0 else -1
+                if abs(x_dist) > abs(y_dist):
+                    if old[(x + x_move), y] == CELL_EMP:
+                        new[(x + x_move), y] = CELL_PED
+                        old[(x + x_move), y] = CELL_OBS
+                    elif old[x, (y + y_move)] == CELL_EMP:
+                        new[x, (y + y_move)] = CELL_PED
+                        old[x, (y + y_move)] = CELL_OBS
                     else:
                         new[x, y] = CELL_PED
                         old[x, y] = CELL_OBS
+                else:
+                    if old[x, (y + y_move)] == CELL_EMP:
+                        new[x, (y + y_move)] = CELL_PED
+                        old[x, (y + y_move)] = CELL_OBS
+                    elif old[(x + x_move), y] == CELL_EMP:
+                        new[(x + x_move), y] = CELL_PED
+                        old[(x + x_move), y] = CELL_OBS
+                    else:
+                        new[x, y] = CELL_PED
+                        old[x, y] = CELL_OBS
+
 
 time = 0
 dens = []
@@ -54,8 +71,8 @@ dens = []
 colors = ["black", "white", "green"]
 cmap = ListedColormap(colors)
 plt.ion()
-# plt.imshow(np.rot90(old, 1))
-plt.imshow(old, cmap=cmap, interpolation='nearest')
+#plt.imshow(old, cmap=cmap, interpolation='nearest')
+plt.imshow(old[100:50, 1:100], cmap=cmap, interpolation='nearest')
 plt.pause(VIS_PAUSE)
 while time < MAX_TIME:
     new = emptyMap.copy()
@@ -64,8 +81,8 @@ while time < MAX_TIME:
     time = time + 1
     if time%VIS_STEPS == 0:
       plt.clf()
-      # plt.imshow(np.rot90(old, 1))
-      plt.imshow(old, cmap=cmap, interpolation='nearest')
+      #plt.imshow(old, cmap=cmap, interpolation='nearest')
+      plt.imshow(old[100:500, 1:100], cmap=cmap, interpolation='nearest')
       plt.pause(VIS_PAUSE)
 plt.ioff()
 
